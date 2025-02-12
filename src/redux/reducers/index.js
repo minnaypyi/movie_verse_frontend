f95dcec6dd5b0f24f99cddc986ddfa2c88c74d07
minnaypyi <119229935+minnaypyi@users.noreply.github.com>
2025-02-13 00:27:26 +0800

    favorite added

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,11 @@
-import moviesReducer from './moviesReducer';
-import filtersReducer from './filtersReducer';
-import genreReducer from './genreReducer';
-import miscReducer from './miscReducer';
-import peopleReducer from './peopleReducer';
-import searchReducer from './searchReducer';
-import favoritesReducer from './favoritesReducer';
+import moviesReducer from "./moviesReducer";
+import filtersReducer from "./filtersReducer";
+import genreReducer from "./genreReducer";
+import miscReducer from "./miscReducer";
+import peopleReducer from "./peopleReducer";
+import searchReducer from "./searchReducer";
+import favoritesReducer from "./favoritesReducer";
+import watchedReducer from "./watchedReducer";
 
 export default {
   favorites: favoritesReducer,
@@ -13,5 +14,6 @@ export default {
   filters: filtersReducer,
   misc: miscReducer,
   people: peopleReducer,
-  search: searchReducer
+  search: searchReducer,
+  watched: watchedReducer,
 };
